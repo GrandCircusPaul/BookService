815eb99a94540b47fe3a32454ae287cb24374b60
GrandCircusPaul <paul@grandcircus.co>
2015-07-29 22:36:52 -0400

    Add Author and Book models
    created empty MVC WebAPI project and added two classes to the Models
    folder, Author.cs and Book.cs.  To each, code was added.

---
+++ BookService/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Author
+    {
+        public int Id { get; set; } //primary key column of database
+        [Required]
+        public string Name { get; set; }
+    }
+}
