f461de3c29d4cacb91ac36b2a6cdcd9727b81e23
GrandCircusPaul <paul@grandcircus.co>
2015-07-30 07:05:00 -0400

    Added Documentation
    Followed tutorial on this site to add documentation:
    
    http://www.asp.net/web-api/overview/getting-started-with-aspnet-web-api/creating-api-help-pages

--- BookService/Controllers/ValuesController.cs
+++ BookService/Controllers/ValuesController.cs
@@ -4,18 +4,27 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace BookService.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
+        /// <summary>
+        /// Gets some very important data from the server.
+        /// </summary>
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        /// <summary>
+        /// Looks up some data by ID.
+        /// </summary>
+        /// <param name="id">The ID of the data.</param>
+        [ApiExplorerSettings(IgnoreApi = true)]
         public string Get(int id)
         {
             return "value";
